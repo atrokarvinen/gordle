f7f59490c07693dd94ffe47d82cebf5062f333c0
Atro Karvinen <atrok@hotmail.com>
2024-01-28 15:56:57 +0200

    change svelte config

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,18 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [vitePreprocess({})],
-	kit: {
-		adapter: adapter(),
-		prerender: {
-			entries: ['/game/-1']
-		},
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/gordle' : ''
-		}
-	}
+	extensions: ['.svelte'],
+	preprocess: [vitePreprocess()],
+	kit: { adapter: adapter() }
 };
-
 export default config;
