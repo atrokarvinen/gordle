c3969953f6e4fd7d018694a6104c404e3610b359
Atro Karvinen <atrok@hotmail.com>
2025-10-06 14:20:33 +0300

    refactored bar chart series

--- client/src/routes/statistics/stats/WinDistributionChart.svelte
+++ client/src/routes/statistics/stats/WinDistributionChart.svelte
@@ -10,8 +10,8 @@
 	const maxGuesses = 8;
 	const guessArray = Array.from({ length: maxGuesses }, (_, i) => i + 1);
 	const yLabels = [...guessArray, $i18n.t('lose')].reverse();
-	let winData = Array.from({ length: maxGuesses + 1 }, () => 0);
-	let lossData = Array.from({ length: maxGuesses + 1 }, () => 0);
+	let winData: number[][];
+	let lossData: number;
 	$: winData = guessArray.map((guess) => {
 		const gamesWithGuess = filteredGames.filter((game) => {
 			const guessCountMatches = game.guesses?.length === guess;
@@ -19,16 +19,18 @@
 			return guessCountMatches && isWin;
 		});
 		const count = gamesWithGuess.length;
-		return (count / filteredGames.length) * 100;
+		return [guess, (count / filteredGames.length) * 100];
 	});
-	$: lossData = lossData.map((_, i) => {
-		if (i < lossData.length - 1) return 0;
+	$: {
 		const totalPlayed = filteredGames.length;
 		const wins = filteredGames.filter((g) => g.state === 2).length;
 		const totalWinRate = filteredGames.length === 0 ? 0 : (wins / filteredGames.length) * 100;
-		if (totalPlayed === 0) return 0;
-		return 100 - totalWinRate;
-	});
+		if (totalPlayed === 0) {
+			lossData = 0;
+		} else {
+			lossData = 100 - totalWinRate;
+		}
+	}
 
 	const getLost = () => {
 		const totalPlayed = filteredGames.length;
@@ -54,11 +56,6 @@
 		return isLost ? getLost() : getWon(guess);
 	});
 
-	const getColor = (cssVar: string) => {
-		const color = getComputedStyle(document.body).getPropertyValue(cssVar);
-		return `rgba(${color})`;
-	};
-
 	$: {
 		console.log('ylabels:', yLabels);
 		console.log('winData:', winData);
@@ -80,21 +77,10 @@
 				trigger: 'axis',
 				axisPointer: { type: 'shadow' },
 				renderMode: 'richText',
-				// formatter: '{a}, {b}: {c}%',
-				valueFormatter: (value) => (value ? `${(+value).toFixed(2)}%` : '0%')
+				valueFormatter: (value) => (value ? `${(+value).toFixed(1)}%` : '0%')
 			},
-			legend: { textStyle: { color: textColor } },
-			xAxis: {
-				type: 'value',
-				axisLabel: { color: textColor }
-			},
-			yAxis: [
-				{
-					type: 'category',
-					data: yLabels,
-					axisLabel: { color: textColor }
-				}
-			],
+			xAxis: { type: 'category', axisLabel: { color: textColor } },
+			yAxis: [{ type: 'value', axisLabel: { color: textColor } }],
 			series: [
 				{
 					name: $i18n.t('wins'),
@@ -102,7 +88,7 @@
 					stack: 'total',
 					emphasis: { focus: 'series' },
 					color: '#11ba81',
-					data: winData.slice().reverse().slice(0, 8)
+					data: winData
 				},
 				{
 					name: $i18n.t('losses'),
@@ -110,47 +96,11 @@
 					stack: 'total',
 					emphasis: { focus: 'series' },
 					color: '#d31b76',
-					data: [lossData[8]]
+					data: [[$i18n.t('lose'), lossData]]
 				}
 			]
 		};
 	}
 </script>
 
-<div id="chartWrapper" class="h-96" use:echarts={option}>
-	<!-- <Bar
-		data={{
-			datasets: [
-				{ data: winData, backgroundColor: getColor('--color-primary-500') },
-				{ data: lossData, backgroundColor: getColor('--color-error-500') }
-			],
-			yLabels: yLabels
-		}}
-		options={{
-			plugins: {
-				legend: { display: false },
-				title: { display: true, text: $i18n.t('games_by_guess_count'), color: 'white' }
-			},
-			maintainAspectRatio: false,
-			responsive: true,
-			indexAxis: 'y',
-			backgroundColor: 'yellow',
-			color: 'white',
-			scales: {
-				x: {
-					stacked: true,
-					title: { display: true, text: '%', color: 'white' },
-					border: { color: 'white' },
-					grid: { color: 'white' },
-					ticks: { color: 'white' }
-				},
-				y: {
-					stacked: true,
-					border: { color: 'white' },
-					grid: { color: 'white' },
-					ticks: { color: 'white' }
-				}
-			}
-		}}
-	/> -->
-</div>
+<div id="chartWrapper" class="h-96" use:echarts={option}></div>
